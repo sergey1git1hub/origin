577df02bc4eae512a565cf96a0216cf6160c6c78
SChubuk <nameserhiy@gmail.com>
2017-12-14 16:00:01 +0200

    Changed to allow execution on VM.

--- src/main/java/Methods.java
+++ src/main/java/Methods.java
@@ -148,6 +148,8 @@ public class Methods {
             WebDriverWait waitForTitle = new WebDriverWait(driver, 10);
             waitForTitle.until(ExpectedConditions.titleIs("gbwebphone"));
             Assert.assertEquals(driver.getTitle(), "gbwebphone");
+            WebDriverWait wait = new WebDriverWait(driver, 10);
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#lang_input_label")));
             WebElement language = driver.findElement(By.cssSelector("#lang_input_label"));
             language.click();
             WebElement language_en = driver.findElement(By.xpath("//li[text() = 'English']"));
