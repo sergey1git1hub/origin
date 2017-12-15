a0466362a461f6835631b7a2d2b1e0947a3d3388
SChubuk <nameserhiy@gmail.com>
2017-12-15 14:53:57 +0200

    Changed to allow execution on VM.

---
+++ src/test/java/MavenFailsafePluginTest.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+/**
+ * Created by SChubuk on 15.12.2017.
+ */
+public class MavenFailsafePluginTest {
+
+    @Test
+    public static void test(){
+        System.out.println("Maven failsafe plugin test.");
+    }
+}
